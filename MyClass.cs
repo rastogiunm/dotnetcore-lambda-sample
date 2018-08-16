feb1b0a640cf812e9cf438cdc7e66d081b28ef4a
Rastogi <RastogiPa01@CompanyNet.org>
2018-08-16 09:58:30 -0700

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace MyNamespace
+{
+    public class MyClass
+    {
+    	public object MyFunction()
+    	{
+    		return new { message = "Hello from Lambda - now using dotnet lambda CLI!", automated_build = true, time = DateTime.UtcNow };
+    	}
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnetcore-lambda-sample
+Sample .NET Core function to be deployed to Amazon Lambda
